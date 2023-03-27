4acb42ffa6f92ae7f27afddd9b3681bf3a828d5b
Sarvesh Kesharwani <sarveshkumar5513@gmail.com>
2023-03-27 15:29:36 +0530

    designed a small webpage

--- app.py
+++ app.py
@@ -1,15 +1,39 @@
-# import our flask library 
-from flask import Flask 
+# # import our flask library 
+# from flask import Flask 
 
-# create our app instance         
-app = Flask(__name__)              
+# # create our app instance         
+# app = Flask(__name__)              
 
-# we create our app route as /, we can use something else as well probably "/hello" 
-@app.route("/")                
-def hello():
-    return "Hello World!"  
+# # we create our app route as /, we can use something else as well probably "/hello" 
+# @app.route("/")                
+# def hello():
+#     return "Hello World!"  
   
-# this is to automatically run the app  
-if __name__ == "__main__":         
-    app.run()
+# # this is to automatically run the app  
+# if __name__ == "__main__":         
+#     app.run()
     
+    
+###############################################################################################
+###############################################################################################
+
+
+from flask import Flask, render_template
+app = Flask(__name__)
+
+# two decorators, same function
+@app.route('/')
+@app.route('/index.html')
+def index():
+    return render_template('index.html', the_title='Tiger Home Page')
+
+@app.route('/symbol.html')
+def symbol():
+    return render_template('symbol.html', the_title='Tiger As Symbol')
+
+@app.route('/myth.html')
+def myth():
+    return render_template('myth.html', the_title='Tiger in Myth and Legend')
+
+if __name__ == '__main__':
+    app.run(debug=True)
